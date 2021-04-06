4570b9baa83fb52f7fb609e8044fc3cab493baa3
rogeriodelphi <rogeriodelphi@gmail.com>
2021-04-05 22:55:01 -0300

    Refactoring the code

---
+++ apps/core/templates/core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from apps.core import views as c
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', c.home, name='home'),
+    # path('gallery/', c.GalleryList.as_view(), name='gallery_list'),
+    # path('add/', c.GalleryCreate.as_view(), name='gallery_add'),
+    # path('gallery/<int:pk>/', c.GalleryDetail.as_view(), name='gallery_detail'),
+    path('download/<int:pk>/', c.download, name='download'),
+]
--- image_gallery/settings.py
+++ image_gallery/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    'widget_tweaks',
+    'django_extensions',
+    'bootstrapform',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +133,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+LOGIN_URL = '/admin/login/'
